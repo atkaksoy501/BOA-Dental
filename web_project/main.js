c347aba39a885ca408230f86bcaf4b034df86080
boerten <boerten@hotech.club>
2023-12-12 15:40:17 +0300

    update project

---
+++ web_project/main.js
@@ -0,0 +1,21 @@
+const imgcontainer = document.querySelector(".image-container");
+const prev = document.getElementById("back");
+const next = document.getElementById("next");
+
+let a = 0;
+let timeout;
+
+prev.addEventListener("click", () => {
+  a = a + 45;
+  updategellary();
+});
+
+next.addEventListener("click", () => {
+  a = a - 45;
+  updategellary();  
+});
+
+function updategellary() {
+  imgcontainer.style.transform = `perspective(1000px) rotateY(${a}deg)`;
+}
+updategellary();
