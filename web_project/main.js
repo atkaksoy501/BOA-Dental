1b409e5de62d4e574cb7d784880ee64964d05a6b
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2023-12-13 18:31:31 +0300

    make an appointment button now have a function

--- web_project/main.js
+++ web_project/main.js
@@ -26,3 +26,26 @@ faqs.forEach(faq => {
     faq.classList.toggle("active");
   })
 })
+
+// JavaScript code
+
+// Get the modal and button elements
+var modal = document.getElementById("appointmentModal");
+var appointmentButton = document.getElementById("appointmentButton");
+
+// When the user clicks the button, display the modal
+appointmentButton.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on the close button, close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
