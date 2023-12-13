17478f930c41674579aed66b58b35c1764e36999
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2023-12-14 00:22:10 +0300

    about and dentists page updates

---
+++ web_project/about.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    // Initialize Slick Carousel for the testimonial slider
+    $('.testimonial-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3, // Show three testimonials in one slide
+      slidesToScroll: 1,
+      autoplay: true, // Auto-slide
+      autoplaySpeed: 3000, // Slide every 3 seconds
+      adaptiveHeight: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
+  
