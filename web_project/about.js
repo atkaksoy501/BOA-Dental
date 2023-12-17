ed57b6447f70d4f422af556f10b0ca9d8ab076ee
Atakan Aksoy <atkaksoy501@hotmail.com>
2023-12-17 14:48:45 +0300

    about page improvements

--- web_project/about.js
+++ web_project/about.js
@@ -1,35 +1,21 @@
 $(document).ready(function () {
-    // Initialize Slick Carousel for the testimonial slider
-    $('.testimonial-slider').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3, // Show three testimonials in one slide
-      slidesToScroll: 1,
-      autoplay: true, // Auto-slide
-      autoplaySpeed: 3000, // Slide every 3 seconds
-      adaptiveHeight: false,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-          },
+  // Initialize Slick Carousel for the testimonial slider
+  $('.testimonial-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Show three testimonials in one slide
+    slidesToScroll: 1,
+    autoplay: true, // Auto-slide
+    autoplaySpeed: 3000, // Slide every 3 seconds
+    adaptiveHeight: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
         },
-      ],
-    });
+      },
+    ],
   });
-
-  $(document).ready(function () {
-    $('.navbar-toggler').click(function () {
-      var mainContent = $('#main-content');
-      var navHeight = $('.navbar').outerHeight();
-
-      if (mainContent.hasClass('menu-open')) {
-        mainContent.removeClass('menu-open').css('margin-top', 0);
-      } else {
-        mainContent.addClass('menu-open').css('margin-top', 125);
-      }
-    });
-  });
-  
+});  
